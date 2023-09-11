1d81d14e64778f36370152c2745ec6b2c56b5ffe
Marcin Moskała <marcinmoskala@gmail.com>
2023-09-11 08:55:43 +0200

    Add all exercises from Functional Kotlin and Advanced Kotlin workshops

---
+++ src/main/kotlin/essentials/classes/Product.kt
@@ -0,0 +1,24 @@
+package essentials.classes
+
+fun main() {
+//    val laptop = Product("Laptop", 999.99, 5)
+//
+//    println(laptop.name) // Laptop
+//    println(laptop.quantity) // 5
+//    println(laptop.calculateTotalValue()) // 4999.95
+//
+//    laptop.restock(3)
+//    
+//    println(laptop.quantity) // 8
+//    println(laptop.calculateTotalValue()) // 7999.92
+//    
+//    laptop.quantity = -2
+//    
+//    println(laptop.quantity) // 0
+//    println(laptop.calculateTotalValue()) // 0.0
+//    
+//    laptop.quantity = 10
+//    
+//    println(laptop.quantity) // 10
+//    println(laptop.calculateTotalValue()) // 9999.9
+}
