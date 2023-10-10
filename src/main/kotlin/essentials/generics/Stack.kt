f436b9a30958947d39976de49b7c906d13a088e0
Marcin Moskała <marcinmoskala@gmail.com>
2023-10-10 13:46:09 +0200

    Fix starting point

--- src/main/kotlin/essentials/generics/Stack.kt
+++ src/main/kotlin/essentials/generics/Stack.kt
@@ -5,46 +5,28 @@ import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class Stack<T> {
-    private val elements: MutableList<T> = mutableListOf()
-
-    fun push(item: T) {
-        elements.add(item)
-    }
-
-    fun pop(): T? = 
-        if (isEmpty()) null 
-        else elements.removeAt(elements.size - 1)
-
-    fun peek(): T? = elements.lastOrNull()
-
-    fun isEmpty(): Boolean = elements.isEmpty()
-
-    fun size(): Int = elements.size
-}
-
 fun main() {
-    val intStack = Stack<Int>()
-    intStack.push(1)
-    intStack.push(2)
-    intStack.push(3)
-
-    val stringStack = Stack<String>()
-    stringStack.push("A")
-    stringStack.push("B")
-    stringStack.push("C")
-
-    println(intStack.peek()) // 3
-    while (!intStack.isEmpty()) { // 3, 2, 1
-        println(intStack.pop())
-    }
-    println(intStack.peek()) // null
-    println(intStack.isEmpty()) // true
-
-    println(stringStack.size()) // 3
-    while (!stringStack.isEmpty()) { // C, B, A
-        println(stringStack.pop()) 
-    }
+//    val intStack = Stack<Int>()
+//    intStack.push(1)
+//    intStack.push(2)
+//    intStack.push(3)
+//
+//    val stringStack = Stack<String>()
+//    stringStack.push("A")
+//    stringStack.push("B")
+//    stringStack.push("C")
+//
+//    println(intStack.peek()) // 3
+//    while (!intStack.isEmpty()) { // 3, 2, 1
+//        println(intStack.pop())
+//    }
+//    println(intStack.peek()) // null
+//    println(intStack.isEmpty()) // true
+//
+//    println(stringStack.size()) // 3
+//    while (!stringStack.isEmpty()) { // C, B, A
+//        println(stringStack.pop()) 
+//    }
 }
 
 class StackTest {
