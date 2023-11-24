4ea0d47dd7b98f8c56eac8acdec816d8cdf4be91
Marcin Moskała <marcinmoskala@gmail.com>
2023-11-24 11:07:23 +0100

    Improve tests

--- src/main/kotlin/essentials/operators/Money.kt
+++ src/main/kotlin/essentials/operators/Money.kt
@@ -1,13 +1,14 @@
 package essentials.operators
 
+import functional.collections.assertThrows
+import org.junit.Test
 import java.math.BigDecimal
+import kotlin.test.assertEquals
 
 data class Money(
-    val amount: BigDecimal, 
-    val currency: Currency
+    val amount: BigDecimal,
+    val currency: Currency,
 ) {
-    // TODO: Implement operators overloading here
-    
     companion object {
         fun eur(amount: String) = Money(BigDecimal(amount), Currency.EUR)
     }
@@ -25,4 +26,58 @@ fun main() {
 //    println(money2 - money1) // Money(amount=19.99, currency=EUR)
 //    println(-money1) // Money(amount=-10.00, currency=EUR)
 //    println(money1 * 3) // Money(amount=30.00, currency=EUR)
+}
+
+class MoneyOperationsTest {
+//    @Test
+//    fun `Money can be added`() {
+//        val money1 = Money.eur("10.00")
+//        val money2 = Money.eur("29.99")
+//        assertEquals(Money.eur("39.99"), money1 + money2)
+//    }
+//
+//    @Test
+//    fun `Money throws exception when adding different currencies`() {
+//        for (currency in Currency.entries) {
+//            for (otherCurrency in (Currency.entries - currency)) {
+//                val money1 = Money(BigDecimal("10.00"), currency)
+//                val money2 = Money(BigDecimal("29.99"), otherCurrency)
+//                assertThrows<IllegalArgumentException> { money1 + money2 }
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun `Money can be subtracted`() {
+//        val money1 = Money.eur("10.00")
+//        val money2 = Money.eur("29.99")
+//        assertEquals(Money.eur("19.99"), money2 - money1)
+//    }
+//
+//    @Test
+//    fun `Money throws exception when subtracting different currencies`() {
+//        for (currency in Currency.entries) {
+//            for (otherCurrency in (Currency.entries - currency)) {
+//                val money1 = Money(BigDecimal("10.00"), currency)
+//                val money2 = Money(BigDecimal("29.99"), otherCurrency)
+//                assertThrows<IllegalArgumentException> { money1 - money2 }
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun `Money can be negated`() {
+//        assertEquals(Money.eur("-10.00"), -Money.eur("10.00"))
+//        assertEquals(Money.eur("-100.00"), -Money.eur("100.00"))
+//        assertEquals(Money.eur("10.00"), -Money.eur("-10.00"))
+//        assertEquals(Money.eur("-999.999"), -Money.eur("999.999"))
+//        
+//    }
+//
+//    @Test
+//    fun `Money can be multiplied by a number`() {
+//        assertEquals(Money.eur("30.00"), Money.eur("10.00") * 3)
+//        assertEquals(Money.eur("0.00"), Money.eur("10.00") * 0)
+//        assertEquals(Money.eur("-30.00"), Money.eur("10.00") * -3)
+//    }
 }
