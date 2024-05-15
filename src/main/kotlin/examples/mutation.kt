138cdf0ba4b95eaa05a9f138ba170cc82078efbb
Marcin Moskała <marcinmoskala@gmail.com>
2024-05-15 14:17:06 +0200

    Update

--- src/main/kotlin/examples/mutation.kt
+++ src/main/kotlin/examples/mutation.kt
@@ -1,23 +1,56 @@
 package examples.mutation
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import kotlin.test.assertEquals
+
 data class User(val name: String)
 
 class UserRepository {
-    private val storedUsers: MutableMap<Int, String> = mutableMapOf()
+   private val storedUsers: MutableMap<Int, String> = mutableMapOf()
 
-    fun loadAll() = storedUsers
-
-    fun add(id: Int, name: String) {
-        storedUsers[id] = name
-    }
-
-    //...
+   fun loadAll() = storedUsers
+  
+   fun add(id: Int, name: String) {
+       storedUsers[id] = name
+   }
+   //...
 }
 
 fun main() {
-    val repository = UserRepository()
-    repository.add(1, "Alice")
-    repository.add(2, "Bob")
-    val users = repository.loadAll()
-    println(users)
+//    val repo = UserRepository()
+//    val users = repo.loadAll()
+//    users[1] = "ABC" // Should not be possible!
+//    println(repo.loadAll()) // {1=ABC}
+}
+
+class UserRepositoryTest {
+    @Test
+    fun shouldNotLeakAccessToInternalList() {
+        val userRepository = UserRepository()
+        val storedUsers = userRepository.loadAll()
+        userRepository.add(1, "ABC")
+        assertEquals(emptyMap(), storedUsers)
+    }
+
+    @Test
+    fun shouldAllowConcurrentAccess() = runBlocking(Dispatchers.IO) {
+        val userRepository = UserRepository()
+        val usersNum = 10_000
+        coroutineScope {
+            repeat(usersNum) {
+                launch { userRepository.add(it, "User$it") }
+                if (it % 100 == 0) {
+                    launch {
+                        val all = userRepository.loadAll()
+                        all.forEach { (id, name) -> assertEquals("User$id", name) }
+                    }
+                }   
+            }
+        }
+        assertEquals(usersNum, userRepository.loadAll().size)
+    }
 }
