5067ca7eb85cdfba3f9456553db0b826b2bb215c
Marcin Moskała <marcinmoskala@gmail.com>
2024-08-21 12:05:16 +0200

    Correction

--- src/main/kotlin/functional/Check.kt
+++ src/main/kotlin/functional/Check.kt
@@ -1,29 +1,12 @@
-import kotlin.reflect.KProperty
+package functional
 
-private class LoggingProperty<T>(
-    var value: T
-) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): T {
-        println("${prop.name} from $thisRef getter returned $value")
-        return value
-    }
-
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, newValue: T) {
-        println("${prop.name} from $thisRef changed from $value to $newValue")
-        value = newValue
-    }
-}
-class ABC {
-    var token: String? by LoggingProperty(null)
-}
-var attempts: Int by LoggingProperty(0)
+infix fun <A, B, C> ((A) -> B).then(f: (B) -> C): (A) -> C = { f(this(it)) }
+infix fun ((Int) -> Int).thenAdd(value: Int): (Int) -> Int = { this(it) + value }
 
 fun main() {
-    val a = ABC()
-    a.token = "AAA" // token changed from null to AAA
-    val res = a.token // token getter returned AAA
-    println(res) // AAA
-    attempts++
-    // attempts getter returned 0
-    // attempts changed from 0 to 1
+    val double = { i: Int -> i * 2 }
+    val triple = { i: Int -> i * 3 }
+    val square = { i: Int -> i * i }
+    val function = double then triple then square thenAdd 6
+    println(function(1))
 }
