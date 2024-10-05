f9315e52aa8af72d9bab1cf0b7d7786346250058
Marcin Moskała <marcinmoskala@gmail.com>
2024-10-05 21:25:06 +0200

    Add unit tests to Product exercise

--- src/main/kotlin/essentials/classes/Product.kt
+++ src/main/kotlin/essentials/classes/Product.kt
@@ -1,5 +1,8 @@
 package essentials.classes.product
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
 // TODO
 
 fun main() {
@@ -23,4 +26,44 @@ fun main() {
     //
     //println(laptop.quantity) // 10
     //println(laptop.calculateTotalValue()) // 9999.9
+}
+
+class ProductTest {
+//   @Test
+//   fun `should keep name, price and quantity`() {
+//       val product = Product("Apple", 1.0, 10)
+//       assertEquals("Apple", product.name)
+//       assertEquals(1.0, product.price)
+//       assertEquals(10, product.quantity)
+//   }
+// 
+//   @Test
+//   fun `should calculate total value`() {
+//       assertEquals(10.0, Product("Apple", 1.0, 10).calculateTotalValue())
+//       assertEquals(0.0, Product("Apple", 1.0, 0).calculateTotalValue())
+//       assertEquals(0.0, Product("Apple", 0.0, 10).calculateTotalValue())
+//       assertEquals(3.6, Product("Apple", 1.2, 3).calculateTotalValue())
+//   }
+// 
+//   @Test
+//   fun `should restock`() {
+//       val product = Product("Apple", 1.0, 10)
+//       product.restock(5)
+//       assertEquals(15, product.quantity)
+//       product.restock(-5)
+//       assertEquals(15, product.quantity)
+//       product.restock(0)
+//       assertEquals(15, product.quantity)
+//   }
+// 
+//   @Test
+//   fun `should not allow negative quantity`() {
+//       val product = Product("Apple", 1.0, 10)
+//       product.quantity = -5
+//       assertEquals(0, product.quantity)
+//       product.quantity = 8
+//       assertEquals(8, product.quantity)
+//       product.quantity = 0
+//       assertEquals(0, product.quantity)
+//   }
 }
