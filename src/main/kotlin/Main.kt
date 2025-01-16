2bf48f455ec2074f8379130dc0acd4d099a870d4
Marcin Moskała <marcinmoskala@gmail.com>
2025-01-16 08:38:00 +0100

    Correction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,3 @@
-import com.google.errorprone.annotations.Immutable
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.hours
-import kotlin.time.Duration.Companion.minutes
-import kotlin.time.Duration.Companion.seconds
-import kotlin.time.toJavaDuration
-
-@JvmInline value class UserId(val value: Int)
-@JvmInline value class TransactionId(val value: Int)
-
 fun main() {
-    val duration: Duration = 1.hours + 30.minutes + 15.seconds
-    setDuration(duration)
-    
-    fetchUser()
-        .onSuccess { user -> println("User: $user") }
-        .onFailure { error -> println("Error: $error") }
+    println("Hello, World!")
 }
-
-fun fetchUser(): Result<User> = Result.success(User(1, "Alice"))
-
-data class User(val id: Int, val name: String)
-
-fun setDuration(duration: Duration) {
-    val javaDuration = duration.toJavaDuration()
-    println(javaDuration)
-}
-
