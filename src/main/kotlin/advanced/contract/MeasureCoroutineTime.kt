c5e0b54a0a015ba702ea7b8d371d3f83412bca0d
Marcin Moskała <marcinmoskala@gmail.com>
2025-02-26 13:50:59 +0100

    Update to CRLF, minor corrections

---
+++ src/main/kotlin/advanced/contract/MeasureCoroutineTime.kt
@@ -0,0 +1,53 @@
+package advanced.contract.measurecoroutinetime
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.coroutines.coroutineContext
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+suspend fun measureCoroutine(
+    body: suspend () -> Unit
+): Duration {
+    val dispatcher = coroutineContext[ContinuationInterceptor]
+    return if (dispatcher is TestDispatcher) {
+        val before = dispatcher.scheduler.currentTime
+        body()
+        val after = dispatcher.scheduler.currentTime
+        after - before
+    } else {
+        measureTimeMillis {
+            body()
+        }
+    }.milliseconds
+}
+
+suspend fun main() {
+    //runTest {
+    //    val result: String
+    //    val duration = measureCoroutine {
+    //        delay(1000)
+    //        result = "OK"
+    //    }
+    //    println(duration) // 1000 ms
+    //    println(result) // OK
+    //}
+    //
+    //runBlocking {
+    //    val result: String
+    //    val duration = measureCoroutine {
+    //        delay(1000)
+    //        result = "OK"
+    //    }
+    //    println(duration) // 1000 ms
+    //    println(result) // OK
+    //}
+}
