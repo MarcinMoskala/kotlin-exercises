f6b206bd246a89c12bdf18033faf780c0cf4029e
marcinmoskala <marcinmoskala@gmail.com>
2026-05-25 11:37:15 +0200

    Undo exercise

--- src/main/kotlin/essentials/conditions/Tree.kt
+++ src/main/kotlin/essentials/conditions/Tree.kt
@@ -3,15 +3,28 @@ package essentials.tree
 import org.junit.Test
 import kotlin.test.assertEquals
 
-sealed class Tree {
-    override fun toString(): String = when (this) {
-        is Leaf -> value
-        is Node -> "$left, $right"
+// TODO: Clean it up
+abstract class Tree {
+    override fun toString(): String {
+        return treeToString(this, StringBuilder()).toString()
     }
 }
 
 class Leaf(val value: String) : Tree()
 class Node(val left: Tree, val right: Tree) : Tree()
+
+private fun treeToString(tree: Tree, sb: StringBuilder): StringBuilder {
+    if (tree is Leaf) {
+        val leaf = tree as Leaf
+        sb.append(leaf.value)
+    } else if (tree is Node) {
+        val node = tree as Node
+        treeToString(node.left, sb)
+        sb.append(", ")
+        treeToString(node.right, sb)
+    }
+    return sb
+}
 
 class TreeTest {
     @Test
